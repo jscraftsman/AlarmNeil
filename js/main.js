73127765b8e430d98d08c50a353f330bd44c6588
Gervel Giva <gervelgiva@gmail.com>
2013-01-14 14:28:41 +0800

    incomplete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 $(function() {
+	$input = $("#string");
+	$hours = $("#hours");
+	$minutes = $("#minutes");
+	$list = $("#list");
 	
+	$("#set").click(function(){
+		var string = $input.val(),
+		hour = $hours.val(),
+		minute = $minutes.val();
+		$list.append(string + " - " + hour + " : " + minute + "\n");
+	});
+	
+	$("#reset").click(function(){
+		resetInputs();
+		$("#list").val("");
+		
+	});
+	function resetInputs(){
+		$input = $("#string").val("");
+		$hours = $("#hours").val(0);
+		$minutes = $("#minutes").val(0);
+	}
 });
+
 
 function hasLocalStorage() {
   try {
